1a5a2de63a7bb06fad1592184638d8235a72f6e2
yashdargude <yashdargude567@gmail.com>
2025-03-04 13:39:51 +0530

    minor changes

--- Backend-main/Helpers/AddMessage.js
+++ Backend-main/Helpers/AddMessage.js
@@ -1,19 +1,22 @@
-
-const Message = require('../Models/Message')
+const Message = require("../Models/Message");
 async function addmessage(messageData) {
-    const new_message = new Message({
-        messagedata: messageData.text,
-        user_id: messageData.user_id
-    })
+  const new_message = new Message({
+    messagedata: messageData.text,
+    user_id: messageData.user_id,
+  });
 
-    await new_message.save()
+  await new_message.save();
 
-    const data = await Message.findOne({ _id: new_message._id }).populate("user_id");
+  const data = await Message.findOne({ _id: new_message._id }).populate(
+    "user_id"
+  );
 
-    // // console.log(allmessage);
-    return data;
+  // // console.log(allmessage);
+  return data;
 }
 
 module.exports = {
-    addmessage
-}
+  addmessage,
+};
+
+//module exports the addmessage function
